07a4801ab8afce56940f28c95c91496c95885ea4
A11eyCat123 <lionsmanerabbit@gmail.com>
2025-03-11 21:49:00 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export default defineConfig({
-  // https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "./",
@@ -18,5 +11,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   }
-})
-}); 
+});
