c642768dde43a32de7944cda09100daeb20af334
A11eyCat123 <lionsmanerabbit@gmail.com>
2025-03-16 15:36:04 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -109,12 +109,12 @@ export default {
           "mech": "The Bear"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "FREEBIRD",
+          "alias": "Stan Richardson",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Necrophile"
+          "mech": "Emma"
         },
         {
           "callsign": "Starboard",
