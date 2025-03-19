456813c8a61dcdf10c0862fe8291a0db5f861c31
Ham123-code <happilyeverackley0604@gmail.com>
2025-03-19 16:50:15 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -142,7 +142,7 @@ export default {
         },
         {
           "callsign": "Map",
-          "alias": 'Recommisions Facility J-Bravo',
+          "alias": 'Recommission Facility J-Bravo',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Everest",
@@ -151,7 +151,7 @@ export default {
          {
           "callsign": "Lieutenant",
           "alias": "M17C",
-          "code": "REDACTED-IMOLATECLEARENCEGIVENATCONFLICT279-111",
+          "code": "UNK0WN",
           "corpro": "GMS",
           "frame": "Everest",
           "mech": "The Bear"
